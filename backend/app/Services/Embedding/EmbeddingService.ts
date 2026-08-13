450d013d5388cf3453ccada2706fa76195d85229
Nitesh Kumar <niteshmrh@gmail.com>
2026-08-13 22:18:50 +0530

    OpenAi Integration only for prod

--- backend/app/Services/Embedding/EmbeddingService.ts
+++ backend/app/Services/Embedding/EmbeddingService.ts
@@ -1,20 +1,15 @@
 import DocumentRepository from "../../Repositories/DocumentRepository.js";
-import OllamaService from "./OllamaService.js";
+import AIService from "./AIService.js";
 
 const EmbeddingService = {
-
-    async process(documentId: string) {
-        const chunks = await DocumentRepository.findChunks(documentId);
-        for (const chunk of chunks) {
-            const embedding = await OllamaService.embedding(chunk.content);
-            await DocumentRepository.updateEmbedding(
-                chunk.id,
-                embedding,
-            );
-            console.log(`Embedded Chunk ${chunk.chunkIndex}`);
-        }
-    },
-
+  async process(documentId: string) {
+    const chunks = await DocumentRepository.findChunks(documentId);
+    for (const chunk of chunks) {
+      const embedding = await AIService.embedding(chunk.content);
+      await DocumentRepository.updateEmbedding(chunk.id, embedding);
+      console.log(`Embedded Chunk ${chunk.chunkIndex}`);
+    }
+  },
 };
 
 export default EmbeddingService;
--- backend/config/env.ts
+++ backend/config/env.ts
@@ -8,11 +8,21 @@ const env = {
   NODE_ENV: process.env.NODE_ENV || "development",
   APP_NAME: process.env.APP_NAME || "DocMind AI",
   API_PREFIX: process.env.API_PREFIX || "/api/v1",
-  DATABASE_URL: process.env.DATABASE_URL || "postgresql://postgres:postgres@localhost:5432/docmind_ai",
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    "postgresql://postgres:postgres@localhost:5432/docmind_ai",
+
   TOKEN_SECRET: process.env.TOKEN_SECRET || "Nitesh_Kumar",
   TOKEN_SECRET_EXPIRES_IN: process.env.TOKEN_SECRET_EXPIRES_IN || "1d",
-  TOKEN_REFRESH_SECRET: process.env.TOKEN_REFRESH_SECRET || "Nitesh_Kumar_Refresh",
+  TOKEN_REFRESH_SECRET:
+    process.env.TOKEN_REFRESH_SECRET || "Nitesh_Kumar_Refresh",
   TOKEN_REFRESH_EXPIRES_IN: process.env.TOKEN_REFRESH_EXPIRES_IN || "1d",
+
+  AI_PROVIDER: process.env.AI_PROVIDER || "ollama",
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
+  OPENAI_CHAT_MODEL: process.env.OPENAI_CHAT_MODEL || "gpt-5-mini",
+  OPENAI_EMBEDDING_MODEL:
+    process.env.OPENAI_EMBEDDING_MODEL || "text-embedding-3-small",
 };
 
 export default env;
